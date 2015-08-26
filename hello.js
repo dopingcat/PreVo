00860af45f23b2ad5e64954cada683241b69d76c
vwan <dopingcat@nate.com>
2015-08-26 23:20:45 +0900

    s
    s

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
-var http = require('http'); 
- 
-http.createServer(function (req, res) { 
-    res.writeHead(200, {'Content-Type': 'text/html'}); 
-    res.end('Hello, world! [helloworld sample; iisnode version is ' + process.env.IISNODE_VERSION + ', node version is ' + process.version + ']'); 
-}).listen(process.env.PORT);   
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello, world! [helloworld sample]');
+}).listen(process.env.PORT);
