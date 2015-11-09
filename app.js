a819c08b55676ffe8dbd010f574983bea057d9b7
권대혁 <dopingcat@nate.com>
2015-11-09 19:02:58 +0900

    world to prevo

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, world! [helloworld sample]');
+    res.end('Hello, PreVo! [helloworld sample]');
 }).listen(process.env.PORT);
